52b81a7a651130eaf8c69deffb9e5e7b147a60da
Salemah <kadertanvir1@gmail.com>
2022-05-22 23:36:27 +0600

    first commit

---
+++ cv2/Controllers/CvvController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2.Controllers
+{
+    public class CvvController : Controller
+    {
+        // GET: Cvv
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult mycv()
+        {
+            return View();
+        }
+    }
+}
